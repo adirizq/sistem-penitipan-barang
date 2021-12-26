da311fe600354e7900b187d0508417f0323cb31f
adirizq <rizkyzadadi@gmail.com>
2021-12-26 11:18:50 +0700

    Adding Locker Class

---
+++ sistem-penitipan-barang/Locker.vb
@@ -0,0 +1,16 @@
+﻿Imports System.Text
+Imports MySql.Data.MySqlClient
+
+Public Class Locker
+
+    Public Shared dbConn As New MySqlConnection
+    Public Shared sqlCommand As New MySqlCommand
+    Public Shared sqlRead As MySqlDataReader
+    Private sqlQuery As String
+
+    Private server As String = "127.0.0.1"
+    Private username As String = "root"
+    Private password As String = ""
+    Private database As String = "sistem_penitipan_barang"
+
+End Class
